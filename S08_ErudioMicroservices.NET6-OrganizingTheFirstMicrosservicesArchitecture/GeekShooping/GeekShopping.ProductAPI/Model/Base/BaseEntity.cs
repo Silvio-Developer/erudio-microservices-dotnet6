6a33a19b091e1337352e753e264ec5a75b4bfd67
Silvio-Developer <sgomes.dev@gmail.com>
2022-03-03 20:21:29 -0300

    Organizing The First Microsservices Architecture

---
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShooping/GeekShopping.ProductAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model.Base
+{
+    public class BaseEntity
+    {
+        // Anotação anônima
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+
+    }
+}
